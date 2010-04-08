07a99bb13f0a0bacbdcefba07ceef71e42405cf9
Yoshiharu Fujisawa <yoshiharu.fujisawa@gmail.com>
2010-04-09 06:31:03 +0900

    improved invalid http sequence on head method and test case.

--- dummy-server.scm
+++ dummy-server.scm
@@ -6,6 +6,7 @@
   (use util.list)
   (use rfc.uri)
   (use file.util)
+  (use gauche.uvector)
   (export run-dummy-server))
 
 (select-module dummy-server)
@@ -71,7 +72,8 @@
             (cond [clen (display maybe-body out)]
                   [tenc (put-chunked out maybe-body)]))
           (flush out))
-        (receive (stat-line2 hdrs2 maybe-body2) (http-receive in :get-body #t)
+        (receive (stat-line2 hdrs2 maybe-body2)
+            (http-receive in :head? (equal? method "HEAD"))
           (match-let1 (ver code desc ...) (string-tokenize stat-line2)
             (if (#/^3/ code)
                 (receive (scheme user host port path query frag)
@@ -80,18 +82,19 @@
                 (values stat-line2 hdrs2 maybe-body2))))
         ))))
 
-(define (http-receive iport :key (get-body #f))
+(define (http-receive iport :key (head? #f))
   (define (get-method line)
     (string-scan line " " 'before))
   (let* ((stat-line (read-line iport))
          (hdrs      (rfc822-read-headers iport)))
     (let1 maybe-body (let ((clen (rfc822-header-ref hdrs "content-length"))
                            (tenc (rfc822-header-ref hdrs "transfer-encoding")))
-                       (cond [(and clen
+                       (cond [head? #f]
+                             [(and clen
                                    (not (equal? (get-method stat-line) "HEAD")))
                               (read-block (x->number clen) iport)]
                              [(equal? tenc "chunked") (get-chunked iport)]
-                             [get-body (port->string iport)]
+                             [(#/^HTTP/ stat-line) (port->string iport)]
                              [else #f]))
       (let1 maybe-body (if (eof-object? maybe-body) #f maybe-body)
         (values stat-line hdrs maybe-body)))))
@@ -121,6 +124,16 @@
   (format oport "~x\r\n" (string-length body))
   (format oport body)
   (format oport "\r\n0\r\n\r\n"))
+
+  (define (read-block len iport)
+    (let1 vec (make-u8vector len 0)
+      (let loop ((start 0) (remain len))
+        (if (> len 0)
+            (let1 len (read-block! vec iport start)
+              (if (eof-object? len)
+                  (u8vector->string vec)
+                  (loop (+ start len) (- remain len))))
+            (u8vector->string vec)))))
 
 (define (assoc-get alist key)
   (cond ((assoc key alist) => cadr)
--- test.scm
+++ test.scm
@@ -14,7 +14,8 @@
 (use net.amazon.s3)
 (test-module 'net.amazon.s3)
 
-(define *test-mode* 'replay) ;; 'record, replay or direct
+(define *test-mode* 'direct) ;; 'record, 'replay or 'direct
+(define *load-test* #t)
 
 ;; Test string-to-sign
 ;; Examples taken from http://docs.amazonwebservices.com/AmazonS3/2006-03-01/index.html?RESTAuthentication.html#ConstructingTheAuthenticationHeader
@@ -202,9 +203,8 @@
          (run-dummy-server *test-port* *test-mode*)
          )))
 
-(let ((bucket "yfujisawa2"
-              ;; #`",(sys-time)"
-              )
+(sys-srandom (sys-time))
+(let ((bucket #`",(sys-random)")
       (dummy-server (run-server-by-mode)))
   (define (keys-file-load iport)
     (define (assoc-get lis key)
@@ -215,12 +215,15 @@
                      (port->list read-line iport))
       (values (assoc-get alist "access-key-id")
               (assoc-get alist "secret-access-key"))))
-  (receive (keyid skey) (call-with-input-file "keys.txt" keys-file-load)
-    (when (and keyid skey)
-      (parameterize ((aws-access-key-id     keyid)
-                     (aws-secret-access-key skey))
-        (test*    "bucket-list" '() (s3-bucket-list))
-        (test-raw "bucket-list/raw" "HTTP/1.1 200 OK
+  (dynamic-wind
+      values
+      (lambda ()
+        (receive (keyid skey) (call-with-input-file "keys.txt" keys-file-load)
+          (when (and keyid skey)
+            (parameterize ((aws-access-key-id     keyid)
+                           (aws-secret-access-key skey))
+              (test*    "bucket-list" '() (s3-bucket-list))
+              (test-raw "bucket-list/raw" "HTTP/1.1 200 OK
 x-amz-id-2: gyB+3jRPnrkN98ZajxHXr3u7EFM67bNgSAxexeEHndCX/7GRnfTXxReKUQF28IfP
 x-amz-request-id: 3B3C7C725673C630
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -237,18 +240,18 @@ Server: AmazonS3
   </Owner>
   <Buckets/>
 </ListAllMyBucketsResult>"
-                  (s3-bucket-list/raw))
-        (test* #`"bucket-available? ,|bucket| if does not exist"
-               'available
-               (s3-bucket-availability bucket))
-        (test* #`"create bucket ,|bucket|" #t
-               (begin (s3-bucket-create! bucket) #t))
-        (test* #`"bucket-available? ,|bucket| if exists"
-               'used
-               (s3-bucket-availability bucket))
-        (test* #`"bucket-location us-classic" 'us-classic
-               (s3-bucket-location bucket))
-        (test-raw #`"bucket-location/raw us-classic" "HTTP/1.1 200 OK
+                        (s3-bucket-list/raw))
+              (test* #`"bucket-available? ,|bucket| if does not exist"
+                     'available
+                     (s3-bucket-availability bucket))
+              (test* #`"create bucket ,|bucket|" #t
+                     (begin (s3-bucket-create! bucket) #t))
+              (test* #`"bucket-available? ,|bucket| if exists"
+                     'used
+                     (s3-bucket-availability bucket))
+              (test* #`"bucket-location us-classic" 'us-classic
+                     (s3-bucket-location bucket))
+              (test-raw #`"bucket-location/raw us-classic" "HTTP/1.1 200 OK
 x-amz-id-2: gyB+3jRPnrkN98ZajxHXr3u7EFM67bNgSAxexeEHndCX/7GRnfTXxReKUQF28IfP
 x-amz-request-id: 3B3C7C725673C630
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -259,12 +262,12 @@ Server: AmazonS3
 
 <?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <LocationConstraint xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\"/>"
-                  (s3-bucket-location/raw bucket))
-        (test* #`"delete bucket ,|bucket|" #t
-               (begin (s3-bucket-delete! bucket) #t))
+                        (s3-bucket-location/raw bucket))
+              (test* #`"delete bucket ,|bucket|" #t
+                     (begin (s3-bucket-delete! bucket) #t))
 
-        (let1 bucket #`",|bucket|2"
-          (test-raw #`"create-bucket/raw! ,|bucket|" "HTTP/1.1 200 OK
+              (let1 bucket #`",|bucket|2"
+                (test-raw #`"create-bucket/raw! ,|bucket|" "HTTP/1.1 200 OK
 x-amz-id-2: YgIPIfBiKa2bj0KMg95r/0zo3emzU4dzsD4rcKCHQUAdQkf3ShJTOOpXUueF6QKo
 x-amz-request-id: 236A8905248E5A01
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -272,10 +275,10 @@ Location: /colorpictures
 Content-Length: 0
 Connection: close
 Server: AmazonS3"
-                    (s3-bucket-create/raw! bucket :location "EU"))
-          (test* #`"bucket-location EU" 'EU
-                 (s3-bucket-location bucket))
-          (test-raw #`"bucket-location/raw EU" "HTTP/1.1 200 OK
+                          (s3-bucket-create/raw! bucket :location "EU"))
+                (test* #`"bucket-location EU" 'EU
+                       (s3-bucket-location bucket))
+                (test-raw #`"bucket-location/raw EU" "HTTP/1.1 200 OK
 x-amz-id-2: gyB+3jRPnrkN98ZajxHXr3u7EFM67bNgSAxexeEHndCX/7GRnfTXxReKUQF28IfP
 x-amz-request-id: 3B3C7C725673C630
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -288,8 +291,8 @@ Server: AmazonS3
 <LocationConstraint xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
 EU
 </LocationConstraint>"
-                    (s3-bucket-location/raw bucket))
-          (test-raw #`"object-list/raw ,|bucket|" #`"HTTP/1.1 200 OK
+                          (s3-bucket-location/raw bucket))
+                (test-raw #`"object-list/raw ,|bucket|" #`"HTTP/1.1 200 OK
 x-amz-id-2: gyB+3jRPnrkN98ZajxHXr3u7EFM67bNgSAxexeEHndCX/7GRnfTXxReKUQF28IfP
 x-amz-request-id: 3B3C7C725673C630
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -306,13 +309,13 @@ Server: AmazonS3
   <MaxKeys>1000</MaxKeys>
   <IsTruncated>false</IsTruncated>
 </ListBucketResult>"
-                    (s3-object-list/raw bucket))
-          (test* #`"object-put! ,|bucket| test-obj.txt"
-                 (s3-object-put! bucket "test-obj.txt" "this is a test.")
-                 (rfc822-header-ref (s3-object-head bucket "test-obj.txt")
-                                    "etag"))
-          
-          (test-raw "object-put/raw!" "HTTP/1.1 200 OK
+                          (s3-object-list/raw bucket))
+                (test* #`"object-put! ,|bucket| test-obj.txt"
+                       (s3-object-put! bucket "test-obj.txt" "this is a test.")
+                       (rfc822-header-ref (s3-object-head bucket "test-obj.txt")
+                                          "etag"))
+                
+                (test-raw "object-put/raw!" "HTTP/1.1 200 OK
 x-amz-id-2: LriYPLdmOdAiIfgSm/F1YsViT1LW94/xUQxMsF7xiEb1a0wiIOIxl+zbwZ163pt7
 x-amz-request-id: 0A49CE4060975EAC
 Date: Wed, 12 Oct 2009 17:50:00 GMT
@@ -320,10 +323,10 @@ ETag: \"1b2cf535f27731c974343645a3985328\"
 Content-Length: 0
 Connection: close
 Server: AmazonS3"
-                    (s3-object-put/raw! bucket "test-obj2.txt" "this is a test."))
-          (test* #`"object-get ,|bucket| test-obj.txt" "this is a test."
-                 (s3-object-get bucket "test-obj.txt"))
-          (test-raw #`"object-get/raw ,|bucket| test-obj.txt" "HTTP/1.1 200 OK
+                          (s3-object-put/raw! bucket "test-obj2.txt" "this is a test."))
+                (test* #`"object-get ,|bucket| test-obj.txt" "this is a test."
+                       (s3-object-get bucket "test-obj.txt"))
+                (test-raw #`"object-get/raw ,|bucket| test-obj.txt" "HTTP/1.1 200 OK
 x-amz-id-2: eftixk72aD6Ap51TnqcoF8eFidJG9Z/2mkiDFu8yU9AS1ed4OpIszj7UDNEHGran
 x-amz-request-id: 318BC8BC148832E5
 Date: Wed, 28 Oct 2009 22:32:00 GMT
@@ -335,8 +338,8 @@ Connection: close
 Server: AmazonS3
 
 this is a test."
-                    (s3-object-get/raw bucket "test-obj.txt"))
-          (test-raw #`"object-list/raw ,|bucket|" #`"HTTP/1.1 200 OK
+                          (s3-object-get/raw bucket "test-obj.txt"))
+                (test-raw #`"object-list/raw ,|bucket|" #`"HTTP/1.1 200 OK
 x-amz-id-2: gyB+3jRPnrkN98ZajxHXr3u7EFM67bNgSAxexeEHndCX/7GRnfTXxReKUQF28IfP
 x-amz-request-id: 3B3C7C725673C630
 Date: Wed, 01 Mar  2009 12:00:00 GMT
@@ -375,27 +378,27 @@ Server: AmazonS3
      </Owner>
   </Contents>
 </ListBucketResult>"
-                    (s3-object-list/raw bucket))
-          (let1 copied-etag (s3-object-copy! bucket #`"/,|bucket|/test-obj.txt"
-                                             "test-obj3.txt")
-            (receive (sxml hdrs) (s3-object-get/raw bucket "test-obj3.txt")
-              (define (filter-token hdrs tokens)
-                (let loop ((hdrs hdrs) (out '()))
-                  (if (null? hdrs)
-                      (reverse out)
-                      (if (member (caar hdrs) tokens)
-                          (loop (cdr hdrs) (cons (car hdrs) out))
-                          (loop (cdr hdrs) out)))))
-              (test* #`"object-head ,|bucket|" (filter-token hdrs '("etag" "last-modified"))
-                     (filter-token (s3-object-head bucket "test-obj3.txt")
-                                   '("etag" "last-modified")))
-              (test* #`"object-head/raw ,|bucket|" (cons #f (filter-token hdrs '("etag" "last-modified")))
-                     (receive (sxml hdrs) (s3-object-head/raw bucket "test-obj3.txt")
-                       (cons sxml (filter-token hdrs '("etag" "last-modified")))))
-              (test* "object-copy!" copied-etag
-                     (cond [(assoc "ETag" hdrs) => cadr]
-                           [else #f]))))
-          (test-raw "object-copy/raw!" "HTTP/1.1 200 OK
+                          (s3-object-list/raw bucket))
+                (let1 copied-etag (s3-object-copy! bucket #`"/,|bucket|/test-obj.txt"
+                                                   "test-obj3.txt")
+                  (receive (sxml hdrs) (s3-object-get/raw bucket "test-obj3.txt")
+                    (define (filter-token hdrs tokens)
+                      (let loop ((hdrs hdrs) (out '()))
+                        (if (null? hdrs)
+                            (reverse out)
+                            (if (member (caar hdrs) tokens)
+                                (loop (cdr hdrs) (cons (car hdrs) out))
+                                (loop (cdr hdrs) out)))))
+                    (test* #`"object-head ,|bucket|" (filter-token hdrs '("etag" "last-modified"))
+                           (filter-token (s3-object-head bucket "test-obj3.txt")
+                                         '("etag" "last-modified")))
+                    (test* #`"object-head/raw ,|bucket|" (cons #f (filter-token hdrs '("etag" "last-modified")))
+                           (receive (sxml hdrs) (s3-object-head/raw bucket "test-obj3.txt")
+                             (cons sxml (filter-token hdrs '("etag" "last-modified")))))
+                    (test* "object-copy!" copied-etag
+                           (cond [(assoc "ETag" hdrs) => cadr]
+                                 [else #f]))))
+                (test-raw "object-copy/raw!" "HTTP/1.1 200 OK
 x-amz-id-2: eftixk72aD6Ap51TnqcoF8eFidJG9Z/2mkiDFu8yU9AS1ed4OpIszj7UDNEHGran
 x-amz-request-id: 318BC8BC148832E5
 Date: Wed, 28 Oct 2009 22:32:00 GMT
@@ -407,37 +410,58 @@ Server: AmazonS3
    <LastModified>2009-10-28T22:32:00</LastModified>
    <ETag>\"9b2cf535f27731c974343645a3985328\"</ETag>
  </CopyObjectResult>"
-                    (s3-object-copy/raw! bucket #`"/,|bucket|/test-obj.txt"
-                                         "test-obj4.txt"))
-          (test* #`"object-list ,|bucket|" '("test-obj.txt" "test-obj2.txt"
-                                             "test-obj3.txt" "test-obj4.txt")
-                 (s3-object-list bucket))
-          (test* #`"object-delete! ,|bucket| test-obj.txt" #t
-                 (begin (s3-object-delete! bucket "test-obj.txt") #t))
-          (test* #`"object-delete! ,|bucket| test-obj2.txt" #t
-                 (begin (s3-object-delete! bucket "test-obj2.txt") #t))
-          (test-raw #`"object-delete/raw! ,|bucket| test-obj3.txt"
-                    "HTTP/1.1 204 NoContent
+                          (s3-object-copy/raw! bucket #`"/,|bucket|/test-obj.txt"
+                                               "test-obj4.txt"))
+                (test* #`"object-list ,|bucket|" '("test-obj.txt" "test-obj2.txt"
+                                                   "test-obj3.txt" "test-obj4.txt")
+                       (s3-object-list bucket))
+                (test* #`"object-delete! ,|bucket| test-obj.txt" #t
+                       (begin (s3-object-delete! bucket "test-obj.txt") #t))
+                (test* #`"object-delete! ,|bucket| test-obj2.txt" #t
+                       (begin (s3-object-delete! bucket "test-obj2.txt") #t))
+                (test-raw #`"object-delete/raw! ,|bucket| test-obj3.txt"
+                          "HTTP/1.1 204 NoContent
 x-amz-id-2: LriYPLdmOdAiIfgSm/F1YsViT1LW94/xUQxMsF7xiEb1a0wiIOIxl+zbwZ163pt7
 x-amz-request-id: 0A49CE4060975EAC
 Date: Wed, 12 Oct 2009 17:50:00 GMT
 Content-Length: 0
 Connection: close
 Server: AmazonS3"
-                    (s3-object-delete/raw! bucket "test-obj3.txt"))
-          (test* #`"object-delete! ,|bucket| test-obj4.txt" #t
-                 (begin (s3-object-delete! bucket "test-obj4.txt") #t))
-          (test* #`"object-list ,|bucket|" '()
-                 (s3-object-list bucket))
-          (test-raw #`"delete-bucket/raw! ,|bucket|" "HTTP/1.1 204 No Content
+                          (s3-object-delete/raw! bucket "test-obj3.txt"))
+                (test* #`"object-delete! ,|bucket| test-obj4.txt" #t
+                       (begin (s3-object-delete! bucket "test-obj4.txt") #t))
+                (test* #`"object-list ,|bucket|" '()
+                       (s3-object-list bucket))
+                (test-raw #`"delete-bucket/raw! ,|bucket|" "HTTP/1.1 204 No Content
 x-amz-id-2: JuKZqmXuiwFeDQxhD7M8KtsKobSzWA1QEjLbTMTagkKdBX2z7Il/jGhDeJ3j6s80
 x-amz-request-id: 32FE2CEB32F5EE25
 Date: Wed, 01 Mar  2009 12:00:00 GMT
 Connection: close
 Server: AmazonS3"
-                    (s3-bucket-delete/raw! bucket)))
-        )))
-  (when dummy-server (sys-kill dummy-server SIGHUP)))
+                          (s3-bucket-delete/raw! bucket)))
+
+              (when *load-test*
+                (let1 bucket #`",(sys-random)"
+                  (s3-bucket-create! bucket)
+                  (dotimes (cnt2 24)
+                    (print cnt2)
+                    (dotimes (cnt 24 1)
+                      (let1 etag (s3-object-put! bucket "test-obj.txt"
+                                                 (make-string (expt 2 cnt) #\x))
+                        (test* #`"upload ,(expt 2 cnt)(2^,|cnt|)bytes data" etag
+                               (rfc822-header-ref (s3-object-head bucket "test-obj.txt")
+                                                  "etag")))))
+                  (dotimes (cnt 16)
+                    (let* ((upload-content (make-string (expt 2 cnt) #\x))
+                           (etag (s3-object-put! bucket "test-obj.txt"
+                                                 upload-content)))
+                      (test* #`"upload content ,(expt 2 cnt)(2^,|cnt|)bytes data" upload-content
+                             (s3-object-get bucket "test-obj.txt"))))
+                  (s3-object-delete! bucket "test-obj.txt")
+                  (s3-bucket-delete! bucket)))
+              ))))
+      (lambda ()
+        (when dummy-server (sys-kill dummy-server SIGHUP)))))
 
 ;; epilogue
 (test-end)
